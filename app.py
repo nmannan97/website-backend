cc94ea229036a9177fde90c273df89ba17303c13
Naeem Mannan <nmannan97@gmail.com>
2025-07-25 09:59:25 -0700

    changing port

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def page_not_found(e):
     return '404: This route does not exist on the API', 404
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8000)
